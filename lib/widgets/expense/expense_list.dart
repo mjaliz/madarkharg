26428cfb21f0308e5bad5681d6b4e3e76c0f43e6
mjaliz <mrph14@gmail.com>
2024-04-26 13:31:20 +0330

    init madarkharg

---
+++ lib/widgets/expense/expense_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:madarkharg/models/expense.dart';
+import 'package:madarkharg/widgets/expense/expense_item.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
