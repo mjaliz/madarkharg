22fa97d264678099de238fae6f465b9219bdc3a2
mjaliz <mrph14@gmail.com>
2024-04-27 14:59:24 +0330

    017 closing the modal manually

--- lib/widgets/expense/new_expense.dart
+++ lib/widgets/expense/new_expense.dart
@@ -9,18 +9,55 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _textController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _textController,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text("Title"),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            maxLength: 5,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: "\$ ",
+              label: Text("Amount"),
+            ),
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_textController.text);
+                  print(_amountController.text);
+                },
+                child: const Text("Save Expense"),
+              ),
+            ],
+          )
         ],
       ),
     );
